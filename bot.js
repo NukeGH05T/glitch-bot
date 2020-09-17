28d53f72a851463f739ed2b0973a4907ecce4b83
NukeGH05T <nugget.dinner.asif@gmail.com>
2020-09-17 07:53:47 -0700

    Init

---
+++ bot.js
@@ -0,0 +1,39 @@
+/*
+ * Discord Bot Builder Bot
+ * Version 1.2.0
+ * Robert Borghese
+ */
+
+const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
+
+if(!process.send) {
+
+Files.initStandalone();
+
+} else {
+
+process.on('message', function(content) {
+	Files.initBotTest(content);
+});
+
+}
+
+//---------------------------------------------------------------------
+// Glitch 24/7
+// Required to let uptime robot waving our bot.
+//---------------------------------------------------------------------
+
+const express = require('express');
+const keepalive = require('express-glitch-keepalive');
+
+const app = express();
+
+app.use(keepalive);
+
+app.get('/', (req, res) => {
+res.json('This bot should be online! Uptimerobot will keep it alive');
+});
+app.get("/", (request, response) => {
+response.sendStatus(200);
+});
+app.listen(process.env.PORT);
